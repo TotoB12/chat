491d70f422dba915bd292e15565141029d5f72f1
TotoB12 <totobeliard@outlook.com>
2024-10-22 20:37:33 +0200

    Refactor server.js to use express.static for serving static files

--- server.js
+++ server.js
@@ -5,37 +5,9 @@ const path = require("path");
 const app = express();
 const PORT = 8000;
 
-const mimeType = {
-  ".html": "text/html",
-  ".js": "text/javascript",
-  ".mjs": "text/javascript",
-  ".css": "text/css",
-};
-
-app.get("*", (req, res) => {
-  console.log(`  ${req.method} ${req.url}`);
-
-  let sanitizedPath = path
-    .normalize(req.path)
-    .replace(/^(\.\.[\/\\])+/, "")
-    .substring(1);
-
-  if (sanitizedPath === "") {
-    sanitizedPath = "index.html";
-  }
-
-  const ext = path.parse(sanitizedPath).ext;
-
-  fs.readFile(sanitizedPath, (err, data) => {
-    if (err) {
-      res.status(404).end();
-    } else {
-      if (mimeType[ext]) {
-        res.setHeader("Content-Type", mimeType[ext]);
-      }
-      res.end(data);
-    }
-  });
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
